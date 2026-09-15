a896768ee874f7012a58c56073c1c1c0711ffbd1
Ayoub Amraoui <aayoubamraoui@gmail.com>
2026-09-15 14:55:20 +0100

    update login/signup page, add form validators and config/add firebase_auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_infraview_pro_2/screens/auth_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_firebase_infraview_pro_2/screens/home_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -18,11 +21,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Firebase',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          if (snapshot.data != null) {
+            return const HomeScreen();
+          }
+
+          return const AuthScreen();
+        },
       ),
-      home: const AuthScreen(),
     );
   }
 }
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,35 +1,184 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_firebase_infraview_pro_2/screens/sign-up.dart';
-import 'package:flutter_firebase_infraview_pro_2/screens/sign_in.dart';
 
-class AuthScreen extends StatelessWidget {
+import 'home_screen.dart';
+
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
   @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final _keyForm = GlobalKey<FormState>();
+  bool _isLogin = true;
+
+  Future<void> login() async {
+    final isValid = _keyForm.currentState!.validate();
+    if (isValid) {
+      try {
+        final credential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+                email: _emailController.text,
+                password: _passwordController.text);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          if (!mounted) return;
+          showSnackBarMessage('No user found for that email.');
+        } else if (e.code == 'wrong-password') {
+          if (!mounted) return;
+          showSnackBarMessage('Wrong password provided for that user.');
+        } else {
+          if (!mounted) return;
+          showSnackBarMessage(e.message.toString());
+        }
+      }
+    } else {
+      showSnackBarMessage('Inputs not valid!');
+    }
+  }
+
+  Future<void> signup() async {
+    final isValid = _keyForm.currentState!.validate();
+    if (isValid) {
+      try {
+        final credential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          showSnackBarMessage('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          showSnackBarMessage('The account already exists for that email.');
+        }
+      } catch (e) {
+        print(e);
+      }
+    } else {
+      showSnackBarMessage('The account already exists for that email.');
+    }
+  }
+
+  showSnackBarMessage(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Auth Page"),
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(
-                text: 'LogIn',
-              ),
-              Tab(
-                text: 'Sign-Up',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Auth Screen'),
+        centerTitle: true,
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(10),
+        child: Form(
+          key: _keyForm,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 40),
+                const CircleAvatar(
+                  radius: 60,
+                  child: Icon(
+                    Icons.person,
+                    size: 80,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    label: Text('Email'),
+                    hintText: 'Enter your email.',
+                  ),
+                  controller: _emailController,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Email is required!";
+                    } else if (!value.contains("@")) {
+                      return "Enter a valid email!";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    label: Text('Password'),
+                    hintText: 'Enter your password.',
+                  ),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Password is required!";
+                    } else if (value.length < 8) {
+                      return "Password must contains at least 8 characters!";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(height: 10),
+                if (!_isLogin)
+                  TextFormField(
+                    controller: _confirmPasswordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      label: Text('Confirm Password'),
+                      hintText: 'Enter your password again.',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return "Confirm Password is required!";
+                      } else if (_passwordController.text != value) {
+                        return "Not equals to the password!";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: _isLogin ? login : signup,
+                    child:
+                        _isLogin ? const Text('Login') : const Text('Signup'),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _isLogin = !_isLogin;
+                    });
+                  },
+                  child: !_isLogin
+                      ? const Text('Don\'t have an account? SignUp')
+                      : const Text('Already have an account? SignIn'),
+                ),
+              ],
+            ),
           ),
-        ),
-        body: const TabBarView(
-          children: <Widget>[
-            Center(child: SignInScreen()),
-            Center(child: SignUpScreen()),
-          ],
         ),
       ),
     );
